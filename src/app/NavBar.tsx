c901e84322d869c8a3ffd896caeed358eeb1ea25
博瀚君 <41739624+bohanjun@users.noreply.github.com>
2023-05-31 17:11:47 +0800

    修复了 md 屏幕下 NavBar 渲染不正确的问题

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -14,7 +14,7 @@ export default function NavBar() {
           <span className="navbar-toggler-icon" />
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <ul className="navbar-nav me-auto mb-2 mb-md-0">
             <li className="nav-item">
               <a className="nav-link" href="/">
                 发现
