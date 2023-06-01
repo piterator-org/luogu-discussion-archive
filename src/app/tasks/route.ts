9362067bca1f81752e7c1cee57b8ebb26a4dfe24
汪心禾 <wangxinhe06@gmail.com>
2023-06-01 15:09:39 +0800

    Force dynamic
    https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#dynamic

--- src/app/tasks/route.ts
+++ src/app/tasks/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { emitters, metadata } from "@/lib/fetch";
 
-// eslint-disable-next-line import/prefer-default-export
+export const dynamic = "force-dynamic";
+
 export const GET = () =>
   NextResponse.json({ ok: true, metadata, current: Object.keys(emitters) });
