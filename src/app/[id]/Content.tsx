bb68da13ec2bb9a98270ad7616dd14f5c012ae8c
博瀚君 <41739624+bohanjun@users.noreply.github.com>
2023-06-03 11:25:40 +0800

    Text-break will be better

--- src/app/[id]/Content.tsx
+++ src/app/[id]/Content.tsx
@@ -18,7 +18,7 @@ export default function Content({ content }: { content: string }) {
 
   return (
     <div
-      className="markdown overflow-x-auto"
+      className="markdown text-break"
       ref={contentRef}
       /* eslint-disable-next-line react/no-danger */
       dangerouslySetInnerHTML={{ __html: content }}
