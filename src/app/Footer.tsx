9b4168e91bc9728665914725d3a2bd565f5b2a98
博瀚君 <41739624+bohanjun@users.noreply.github.com>
2023-06-08 22:41:42 +0800

    帖子和楼层计数

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,3 +1,5 @@
+import prisma from "@/lib/prisma";
+
 export default function Footer() {
   return (
     <footer className="py-3 py-md-4 mt-5 bg-body-tertiary">
@@ -14,7 +16,11 @@ export default function Footer() {
             <ul className="list-unstyled small">
               <li className="mb-2">
                 全新的洛谷讨论保存站由 Piterator
-                打造，拥有更赏心悦目的界面、更稳定的爬取器、更方便的体验。
+                打造，拥有更赏心悦目的界面、更稳定的爬取器、更方便的体验，我们总是在不断尝试。
+              </li>
+              <li className="mb-2">
+                已经保存了 {prisma.discussion.count()} 个帖子，共计{" "}
+                {prisma.reply.count()} 层楼。
               </li>
               <li className="mb-2">&copy; 2023 Piterator</li>
               <li className="mb-2">v0.0.1 内测版</li>
