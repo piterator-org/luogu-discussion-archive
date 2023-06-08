0b7ed1cb9eca805e8ed77d6c31dc03f10ffdd4d2
汪心禾 <wangxinhe06@gmail.com>
2023-06-08 18:40:31 +0800

    补上 miaomiaowu 以及改成箭头函数

--- src/lib/luogu.ts
+++ src/lib/luogu.ts
@@ -1,29 +1,23 @@
-export function getDiscussionUrl(discussion: number, page?: number) {
-  return `https://www.luogu.com.cn/discuss/${discussion}${
+export const getDiscussionUrl = (discussion: number, page?: number) =>
+  `https://www.luogu.com.cn/discuss/${discussion}${
     page !== undefined ? `?page=${page}` : ""
   }`;
-}
 
-export function getUserUrl(user: number) {
-  return `https://www.luogu.com.cn/user/${user}`;
-}
+export const getUserUrl = (user: number) =>
+  `https://www.luogu.com.cn/user/${user}`;
 
-export function getUserAvatarUrl(user: number) {
-  return `https://cdn.luogu.com.cn/upload/usericon/${user}.png`;
-}
+export const getUserAvatarUrl = (user: number) =>
+  `https://cdn.luogu.com.cn/upload/usericon/${user}.png`;
 
-export function getForumUrl(forum: string) {
-  return `https://www.luogu.com.cn/discuss/lists?forumname=${forum}`;
-}
+export const getForumUrl = (forum: string) =>
+  `https://www.luogu.com.cn/discuss/lists?forumname=${forum}`;
 
-export function getForumName(forum: string) {
-  return (
-    {
-      siteaffairs: "站务版",
-      problem: "题目总版",
-      academics: "学术版",
-      relevantaffairs: "灌水区",
-      service: "反馈、申请、工单专版",
-    }[forum] ?? forum
-  );
-}
+export const getForumName = (forum: string) =>
+  ({
+    siteaffairs: "站务版",
+    problem: "题目总版",
+    academics: "学术版",
+    relevantaffairs: "灌水区",
+    service: "反馈、申请、工单专版",
+    miaomiaowu: "小黑屋",
+  }[forum] ?? forum);
