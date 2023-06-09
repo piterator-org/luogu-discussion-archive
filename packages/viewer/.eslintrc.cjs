7506a0eae3a06461b2a4e5dc34043df4a7b132cc
汪心禾 <wangxinhe06@gmail.com>
2023-06-09 17:29:01 +0800

    Move Next.js into packages/

---
+++ packages/viewer/.eslintrc.cjs
@@ -0,0 +1,42 @@
+const { join } = require("node:path");
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  extends: [
+    "next/core-web-vitals",
+    "airbnb",
+    "plugin:react/jsx-runtime",
+    "plugin:prettier/recommended",
+  ],
+  settings: { next: { rootDir: __dirname } },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      extends: [
+        "airbnb-typescript",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:prettier/recommended",
+      ],
+      settings: {
+        "import/resolver": {
+          typescript: { project: join(__dirname, "tsconfig.json") },
+        },
+      },
+      parserOptions: {
+        project: "tsconfig.json",
+        tsconfigRootDir: __dirname,
+      },
+    },
+    {
+      files: ["*.config.*", ".*rc.*"],
+      rules: {
+        "import/no-extraneous-dependencies": [
+          "error",
+          { devDependencies: true },
+        ],
+      },
+    },
+  ],
+};
--- packages/viewer/next.config.js
+++ packages/viewer/next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    dirs: ["."],
-  },
   images: {
     remotePatterns: [{ protocol: "https", hostname: "cdn.luogu.com.cn" }],
   },
