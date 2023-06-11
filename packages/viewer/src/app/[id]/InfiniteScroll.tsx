f6a8855b83d4b1cbc440688cf904f08de8eb4a58
汪心禾 <wangxinhe06@gmail.com>
2023-06-11 17:37:57 +0800

    优化无限滚动的加载体验

--- packages/viewer/src/app/[id]/InfiniteScroll.tsx
+++ packages/viewer/src/app/[id]/InfiniteScroll.tsx
@@ -18,6 +18,8 @@ type PageData = {
   nextCursor: number;
 };
 
+const REPLIES_PER_PAGE = parseInt(process.env.REPLIES_PER_PAGE ?? "10", 10);
+
 export default function InfiniteScroll({
   discussion,
   pagination,
@@ -35,44 +37,53 @@ export default function InfiniteScroll({
     if (previousPageData && !previousPageData.data) return null;
 
     // 在首页时，没有 `previousPageData`
-    if (pageIndex === 0) return `/${discussion.id}/replies?limit=10`;
+    if (pageIndex === 0)
+      return `/${discussion.id}/replies?limit=${REPLIES_PER_PAGE}`;
 
     // 将游标添加到 API
-    return `/${discussion.id}/replies?cursor=${previousPageData.nextCursor}&limit=10`;
+    return `/${discussion.id}/replies?cursor=${previousPageData.nextCursor}&limit=${REPLIES_PER_PAGE}`;
   };
 
-  const { data, size, setSize } = useSWRInfinite<PageData>(getKey, fetcher);
+  const { data, size, setSize, isValidating, isLoading } =
+    useSWRInfinite<PageData>(getKey, fetcher);
 
   const [showPageButtons, setShowPageButtons] = useState<boolean>(true);
 
   return (
     <>
       {data?.map((replies) =>
-        replies.data.map((reply) => (
+        replies.data?.map((reply) => (
           <Reply discussion={discussion} reply={reply} key={reply.id} />
         ))
       )}
-      {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
-      <div
-        className="bg-body rounded-4 shadow my-4s px-4 py-2x py-md-3 text-center link-primary text-decoration-none cursor-pointer"
-        onClick={() => {
-          // eslint-disable-next-line @typescript-eslint/no-floating-promises
-          setSize(size + 1);
-          setShowPageButtons(false);
-        }}
-      >
-        加载更多
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="1em"
-          height="1em"
-          fill="currentColor"
-          className="bi bi-three-dots ms-1"
-          viewBox="0 0 16 16"
+      {Boolean(
+        (!data?.[data.length - 1].data ||
+          data?.[data.length - 1].data.length < REPLIES_PER_PAGE) &&
+          !isLoading
+      ) || (
+        <button
+          className="btn btn-link w-100 rounded-4 shadow py-2x py-md-3 text-center text-decoration-none"
+          onClick={() => {
+            // eslint-disable-next-line @typescript-eslint/no-floating-promises
+            setSize(size + 1);
+            setShowPageButtons(false);
+          }}
+          disabled={isValidating}
+          type="button"
         >
-          <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
-        </svg>
-      </div>
+          {isValidating ? "加载中" : "加载更多"}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="1em"
+            height="1em"
+            fill="currentColor"
+            className="bi bi-three-dots ms-1"
+            viewBox="0 0 16 16"
+          >
+            <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
+          </svg>
+        </button>
+      )}
       {pagination.numPages > 1 && (
         <div
           className={`bg-body rounded-4 shadow my-4s px-4 py-3 py-md-4 text-center ${
