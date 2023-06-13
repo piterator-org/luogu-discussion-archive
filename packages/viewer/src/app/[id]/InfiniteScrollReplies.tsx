01996b4f96ebe3605374e013b59515d00670b629
博瀚君 <41739624+bohanjun@users.noreply.github.com>
2023-06-13 15:22:31 +0800

    加载更多按钮样式调整

--- packages/viewer/src/app/[id]/InfiniteScrollReplies.tsx
+++ packages/viewer/src/app/[id]/InfiniteScrollReplies.tsx
@@ -93,6 +93,7 @@ export default function InfiniteScrollReplies({
               fill="currentColor"
               className="bi bi-three-dots ms-1"
               viewBox="0 0 16 16"
+              style={{ position: "relative", top: "-.09em" }}
             >
               <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
             </svg>
