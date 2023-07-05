e3e6c33f3243161a22966bb9f51ce5acb071f2bc
汪心禾 <wangxinhe06@gmail.com>
2023-07-05 20:34:51 +0800

    自动保存前五页
    希望不会被封 IP

--- packages/archive/src/plugins/cron.ts
+++ packages/archive/src/plugins/cron.ts
@@ -5,15 +5,27 @@ import delay from "../utils/delay";
 import getDiscussionList from "../lib/list";
 import { emitters, startTask } from "../lib/discussion";
 
-const AUTO_SAVE_INTERVAL = 20000;
+const AUTO_SAVE_PAGES = 5;
+const AUTO_SAVE_INTERVAL = 15000;
 
 export default fastifyPlugin(
   function cron(fastify, options, done) {
     const save: () => Promise<unknown> = () =>
-      (async (after) => [
-        ...(await getDiscussionList(fastify.log, fastify.prisma, 1, after)),
-        ...(await getDiscussionList(fastify.log, fastify.prisma, 2, after)),
-      ])(Math.floor(new Date().getTime() / 1000) - 86400)
+      Array.from(Array(AUTO_SAVE_PAGES))
+        .reduce(
+          (prev: Promise<number[]>, curr, i) =>
+            prev.then(async (array) =>
+              array.concat(
+                await getDiscussionList(
+                  fastify.log,
+                  fastify.prisma,
+                  i + 1,
+                  Math.floor(new Date().getTime() / 1000) - 86400
+                )
+              )
+            ),
+          Promise.resolve([])
+        )
         .then((discussions) => {
           return discussions.reduce(
             (promise: Promise<unknown>, id) =>
