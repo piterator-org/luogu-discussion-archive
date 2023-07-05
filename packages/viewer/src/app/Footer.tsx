7093e18ccd27dcf3205682042f503a2cb99d9922
博瀚君 <41739624+bohanjun@users.noreply.github.com>
2023-07-05 19:47:57 +0800

    页脚 GitHub 链接

--- packages/viewer/src/app/Footer.tsx
+++ packages/viewer/src/app/Footer.tsx
@@ -8,7 +8,7 @@ declare global {
 export default function Footer() {
   return (
     <footer className="py-3 py-md-4 mt-5 bg-body-tertiary">
-      <div className="container-lg py-3 py-md-4 px-4 px-md-3 text-body-secondary">
+      <div className="container-lg py-3 py-md-4 px-4 px-md-3 text-secondary">
         <div className="row">
           <div className="col-lg-3 mb-3">
             <a
@@ -23,7 +23,28 @@ export default function Footer() {
                 全新的洛谷讨论保存站由 Piterator
                 打造，拥有更赏心悦目的界面、更稳定的爬取器、更方便的体验，我们总是在不断尝试。
               </li>
-              <li className="mb-2">&copy; 2023 Piterator</li>
+              <li className="mb-2">
+                &copy; 2023 Piterator &middot;{" "}
+                <a
+                  href="https://github.com/piterator-org/luogu-discussion-archive"
+                  className="link-secondary text-decoration-none"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="1em"
+                    height="1em"
+                    fill="currentColor"
+                    className="bi bi-github position-relative"
+                    viewBox="0 0 16 16"
+                    style={{ top: "-.1em" }}
+                  >
+                    <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                  </svg>{" "}
+                  GitHub
+                </a>
+              </li>
               <li className="mb-2">
                 {VERSION} @{" "}
                 {new Date(LASTCOMMITDATETIME).toLocaleString("zh", {
--- packages/viewer/src/app/user/[uid]/participated/UserParticipated.tsx
+++ packages/viewer/src/app/user/[uid]/participated/UserParticipated.tsx
@@ -204,31 +204,29 @@ export default function UserParticipated({
                                   <span className="d-none d-md-inline">
                                     {reply.time}
                                   </span>
-                                  {reply.id !== undefined ? (
-                                    <Link
-                                      href={`/r/${reply.id}`}
-                                      className="ms-2 link-secondary position-relative"
-                                      style={{ fontSize: ".8em", top: "-.2em" }}
+                                  <Link
+                                    href={`/r/${reply.id}`}
+                                    className="ms-2 link-secondary position-relative"
+                                    style={{ fontSize: ".8em", top: "-.2em" }}
+                                  >
+                                    <svg
+                                      xmlns="http://www.w3.org/2000/svg"
+                                      width="1em"
+                                      height="1em"
+                                      fill="currentColor"
+                                      className="bi bi-box-arrow-up-right"
+                                      viewBox="0 0 16 16"
                                     >
-                                      <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        width="1em"
-                                        height="1em"
-                                        fill="currentColor"
-                                        className="bi bi-box-arrow-up-right"
-                                        viewBox="0 0 16 16"
-                                      >
-                                        <path
-                                          fillRule="evenodd"
-                                          d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"
-                                        />
-                                        <path
-                                          fillRule="evenodd"
-                                          d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"
-                                        />
-                                      </svg>
-                                    </Link>
-                                  ) : undefined}
+                                      <path
+                                        fillRule="evenodd"
+                                        d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"
+                                      />
+                                      <path
+                                        fillRule="evenodd"
+                                        d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"
+                                      />
+                                    </svg>
+                                  </Link>
                                 </span>
                               </div>
                               <div className="px-4 py-2 position-relative">
