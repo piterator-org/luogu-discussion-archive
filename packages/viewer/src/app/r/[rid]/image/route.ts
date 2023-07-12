86946a825371ae781fa0944d7266e2d761931ede
汪心禾 <wangxinhe06@gmail.com>
2023-07-13 01:17:35 +0800

    KaTeX renderer

--- packages/viewer/src/app/r/[rid]/image/route.ts
+++ packages/viewer/src/app/r/[rid]/image/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse, type NextRequest } from "next/server";
 import { notFound } from "next/navigation";
 import puppeteer from "puppeteer";
+import type renderMathInElement from "katex/contrib/auto-render";
+import { autoRender, katex, style } from "./katex";
 
 const browser = await puppeteer.launch();
 
@@ -15,9 +17,26 @@ export async function GET(
   await page.setContent(/* HTML */ `<!DOCTYPE html>
     <html>
       <body>
-        wxhdsb
+        $x=y^2+1$
       </body>
     </html>`);
+
+  await page.addScriptTag({ content: katex as string });
+  await page.addScriptTag({ content: autoRender as string });
+  await page.addStyleTag({ content: style as string });
+  await page.evaluate(() =>
+    (
+      window as typeof window & {
+        renderMathInElement: typeof renderMathInElement;
+      }
+    ).renderMathInElement(document.body, {
+      delimiters: [
+        { left: "$$", right: "$$", display: true },
+        { left: "$", right: "$", display: false },
+      ],
+    })
+  );
+
   const screenshot = await page.screenshot({ fullPage: true });
   await page.close();
   return new NextResponse(screenshot);
