d4d31a1d8d0d7697e74907ff33403e8d18f82b33
汪心禾 <wangxinhe06@gmail.com>
2023-08-13 23:53:18 +0800

    Remove unused request param

--- packages/viewer/src/app/judgement/statistics/route.ts
+++ packages/viewer/src/app/judgement/statistics/route.ts
@@ -1,8 +1,8 @@
-import { NextResponse, type NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
 export const dynamic = "force-dynamic";
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   return NextResponse.json({ judgements: await prisma.judgement.count() });
 }
