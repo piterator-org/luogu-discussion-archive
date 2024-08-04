5c5ef18f121aa6dbfb9704b794222d4def39d346
Imken Luo <me@imken.moe>
2024-08-04 21:37:15 +0800

    make tsc happy

--- packages/archive/src/lib/list.ts
+++ packages/archive/src/lib/list.ts
@@ -72,7 +72,8 @@ export default async function getPostList(
         !(post.id in saved) || // 没有保存过亦如此
         (post.recentReply && // 有回复并
           (!saved[post.id] || // 上一次保存时没有回复或
-            saved[post.id] < post.recentReply.id)), // 存在更新的回复（该 id 随时间单调递增）
+            /* eslint-disable @typescript-eslint/no-non-null-assertion */
+            saved[post.id]! < post.recentReply.id)), // 存在更新的回复（该 id 随时间单调递增）
     )
     .map((post) => post.id);
 }
